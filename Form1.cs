3606fe67bca377ecc1676046766710878c3362a5
KennyKiad <load32103@gmail.com>
2025-06-19 17:34:42 +0300

    Second

--- Form1.cs
+++ Form1.cs
@@ -14,40 +14,55 @@ namespace Calculator
         private void buttonDigitsClick(object sender, EventArgs e)
         {
             var currentButton = sender as Button;
-            textBox.Text += currentButton.Text.ToString();
+
+            if (textBox.Text.Length > 0 && CheckOperatorsNear(textBox.Text))
+            {
+                
+                textBox.Text += $" {currentButton.Text}";
+
+            } else
+            {
+                textBox.Text += currentButton.Text;
+            }
         }
 
         private void buttonOperatorsClick(object sender, EventArgs e)
         {
             var currentButton = sender as Button;
-            var currentText = textBox.Text.Trim();
 
-            if (currentText.Length > 0)
+            if ( textBox.Text.Length > 0 && !CheckOperatorsNear(textBox.Text.Trim()))
             {
-                switch (currentText[currentText.Length - 1])
-                {
-                    case '+':
-                        break;
-                    case '-':
-                        break;
-                    case '*':
-                        break;
-                    case '/':
-                        break;
 
-                    default:
-                        textBox.Text += currentButton.Text.ToString();
-                        break;
-                }
-            } else
+                textBox.Text += currentButton.Text;
+
+            } else if (textBox.Text.Length <= 0)
             {
-                char currentOperator = currentButton.Text.Trim()[0];
-                if (currentOperator == '+' || currentOperator == '-')
+                char currentChar = currentButton.Text.Trim()[0];
+                if (currentChar == '+' || currentChar == '-')
                 {
-                    textBox.Text += currentOperator.ToString();
+                    textBox.Text += currentChar.ToString();
                 }
             }
             
+        }
+
+        private bool CheckOperatorsNear(string currentText)
+        {
+           
+            switch (currentText[currentText.Length - 1])
+            {
+                case '+':
+                    return true;
+                case '-':
+                    return true;
+                case '*':
+                    return true;
+                case '/':
+                    return true;
+
+                default:
+                    return false;
+            }
         }
 
         private void buttonClearClick(object sender, EventArgs e)
@@ -69,22 +84,59 @@ namespace Calculator
         {
             try
             {
-                string expression = textBox.Text.Trim();
-                textBox.Text = "";
-
+                string expression = textBox.Text.Replace(" ", "");
 
                 var result = dataTab.Compute(expression, null);
                 if (result == DBNull.Value || result == null)
                 {
-                    throw new InvalidOperationException("Невозможно вычислить выражение");
+                    throw new InvalidOperationException("Syntax error");
+
                 }
 
                 double resultDouble = Math.Round( Convert.ToDouble(result), 15);
+                if (Double.IsNaN(resultDouble) || 
+                    expression.Contains("/0"))
+                {
+                    throw new InvalidOperationException("Нельзя делить на ноль");
+
+                }
 
                 textBox.Text = resultDouble.ToString().Replace(',', '.');
 
             } catch (Exception ex) {
-                textBox.Text = $"Ошибка: {ex.Message}";
+
+                HandlerError(ex);
+            
+            }
+
+        }
+
+        private void HandlerError(Exception ex)
+        {
+            switch (ex.Message.Contains("Syntax error") || ex.Message.Contains("Cannot") || ex.Message.Contains("correct format"))
+            {
+                case true:
+                    MessageBox.Show($"Невозможно вычислить выражение: {textBox.Text}", "Синтаксическая ошибка", MessageBoxButtons.OK);
+                    break;
+
+                case false:
+
+                    switch (ex.Message)
+                    {
+                        case "The expression has too many closing parentheses.":
+                            MessageBox.Show($"Невозможно вычислить выражение: {textBox.Text}", "Слишком много закрывающих скобок", MessageBoxButtons.OK);
+                            break;
+
+                        case "The expression is missing the closing parenthesis.":
+                            MessageBox.Show($"Невозможно вычислить выражение: {textBox.Text}", "Скобки не закрыты", MessageBoxButtons.OK);
+                            break;
+
+                        case "Нельзя делить на ноль":
+                            MessageBox.Show($"Невозможно вычислить выражение: {textBox.Text}", $"{ex.Message}", MessageBoxButtons.OK);
+                            break;
+
+                    }
+                    break;
             }
         }
     }
